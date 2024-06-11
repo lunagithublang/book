bc7d8f48da71cd10eeba0ad73c987c8fe5acad94
MarkDanielArena <mark.a@projectassistant.io>
2024-06-11 12:08:07 +0800

    Added frontend angular - login - register - activate account - openapi

---
+++ frontend/src/app/services/services/account.service.ts
@@ -0,0 +1,47 @@
+/* tslint:disable */
+/* eslint-disable */
+import { HttpClient, HttpContext } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { map } from 'rxjs/operators';
+
+import { BaseService } from '../base-service';
+import { ApiConfiguration } from '../api-configuration';
+import { StrictHttpResponse } from '../strict-http-response';
+
+import { AuthenticateResponse } from '../models/authenticate-response';
+import { create } from '../fn/account/create';
+import { Create$Params } from '../fn/account/create';
+
+@Injectable({ providedIn: 'root' })
+export class AccountService extends BaseService {
+  constructor(config: ApiConfiguration, http: HttpClient) {
+    super(config, http);
+  }
+
+  /** Path part for operation `create()` */
+  static readonly CreatePath = '/accounts/register';
+
+  /**
+   * This method provides access to the full `HttpResponse`, allowing access to response headers.
+   * To access only the response body, use `create()` instead.
+   *
+   * This method sends `application/json` and handles request body of type `application/json`.
+   */
+  create$Response(params: Create$Params, context?: HttpContext): Observable<StrictHttpResponse<AuthenticateResponse>> {
+    return create(this.http, this.rootUrl, params, context);
+  }
+
+  /**
+   * This method provides access only to the response body.
+   * To access the full response (for headers, for example), `create$Response()` instead.
+   *
+   * This method sends `application/json` and handles request body of type `application/json`.
+   */
+  create(params: Create$Params, context?: HttpContext): Observable<AuthenticateResponse> {
+    return this.create$Response(params, context).pipe(
+      map((r: StrictHttpResponse<AuthenticateResponse>): AuthenticateResponse => r.body)
+    );
+  }
+
+}
