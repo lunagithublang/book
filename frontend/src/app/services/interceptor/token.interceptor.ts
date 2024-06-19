629620afd0ed241483c649f23021cea203f8176b
MarkDanielArena <mark.a@projectassistant.io>
2024-06-19 11:30:04 +0800

    implemented keycloak in frontend modified login/auth guard/menu

--- frontend/src/app/services/interceptor/token.interceptor.ts
+++ frontend/src/app/services/interceptor/token.interceptor.ts
@@ -1,10 +1,14 @@
 import { HttpInterceptorFn } from '@angular/common/http';
 import { TokenService } from '../token/token.service';
 import { inject } from '@angular/core';
+import { KeycloakService } from '../keycloak/keycloak.service';
 
 export const tokenInterceptor: HttpInterceptorFn = (req, next) => {
-  const tokenService = inject(TokenService);
-  const token = tokenService.token;
+  const tokenService: TokenService = inject(TokenService);
+  // const token = tokenService.token;
+
+  const keyCloakService: KeycloakService = inject(KeycloakService);
+  const token = keyCloakService.keyCloak?.token;
 
   if (token) {
     const authRequest = req.clone({
--- frontend/src/app/services/keycloak/keycloak.service.ts
+++ frontend/src/app/services/keycloak/keycloak.service.ts
@@ -1,9 +1,60 @@
-import { Injectable } from '@angular/core';
+import { Injectable, PLATFORM_ID, Inject } from '@angular/core';
+import { isPlatformBrowser } from '@angular/common';
+import Keycloak from 'keycloak-js';
+import { AccountProfile } from './account-profile';
 
 @Injectable({
   providedIn: 'root'
 })
 export class KeycloakService {
 
-  constructor() { }
+  private _keyCloak: Keycloak | undefined;
+  private _accountProfile: AccountProfile | undefined;
+
+  get accountProfile() : AccountProfile | undefined{
+    return this._accountProfile;
+  }
+
+  get keyCloak() {
+    if(!this._keyCloak) {
+      this._keyCloak = new Keycloak({
+        url: 'http://localhost:9090',
+        realm: 'book',
+        clientId: 'book'
+      })
+    }
+    return this._keyCloak
+  }
+
+  constructor(@Inject(PLATFORM_ID) private platformId: Object) { }
+
+  async init () {
+
+    if (!isPlatformBrowser(this.platformId)) {
+      console.log("Not running in the browser, do nothing")
+      return;
+    }
+
+    console.log("Authenticating the user...")
+    const authenticated = await this.keyCloak?.init({
+      onLoad: 'login-required',
+    })
+
+    if (authenticated) {
+      console.log("User authenticated...")
+      this._accountProfile = (await this.keyCloak?.loadUserProfile()) as AccountProfile;
+      this._accountProfile.token = this.keyCloak?.token;
+    }
+  }
+
+  login () {
+    return this.keyCloak?.login();
+  }
+
+  logout () {
+    return this.keyCloak?.logout({
+      redirectUri: 'http://localhost:4200'
+    });
+  }
+
 }
